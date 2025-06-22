4b72febfef27c5d878f5b904034cac3b0cee8452
MrCake1243 <vasya.buzaev.05123@mail.ru>
2025-06-23 02:02:50 +0500

    Delete 1.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-from models import *
-# with db:
-#     products = Product.select()
-#     for p in products:
-#         print(p.id, p.name, p.price, p.category_id.name)
-#         p.price = p.price * 0.9
-#         p.save()
